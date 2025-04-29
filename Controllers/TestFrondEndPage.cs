41633a581c44c6d2ea6e7d707c85fc71d831f14b
gouda123456 <122279160+gouda123456@users.noreply.github.com>
2025-04-29 20:51:48 +0300

    add-Front pages

---
+++ Controllers/TestFrondEndPage.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace e_commerce.Controllers
+{
+    public class TestFrondEndPage : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult betuflpage()
+        {
+            return View();
+        }
+
+        public IActionResult buyProduct()
+        {
+            return View();
+        }
+
+        public IActionResult features()
+        {
+            return View();
+        }
+        public IActionResult mycart()
+        {
+            return View();
+        }
+
+        public IActionResult sign_in_page()
+        {
+            return View();
+        }
+
+        public IActionResult sign_up_page()
+        {
+            return View();
+        }
+    }
+}
